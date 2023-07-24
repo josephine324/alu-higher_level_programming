07d3f54df5b571b867c1a7653986b4e71d29f946
josephine324 <j.mutesi@alustudent.com>
2023-07-24 02:13:14 -0700

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,17 @@
 #!usr/bin/python3
 """
-A file with a positive attribute
+This is a file of a class 
+with an integer attribute
 """
 
 class Square:
     """
-    class with an integer  size attribute
+    This is a class with an integer attribute
     """
-    def __init__(self,size=0):
-        if not (size,int):
-            raise type error ("size must be an integer")
-        elif size<0:
-            raise value error ("size must be >= 0")
-        else :
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = size
