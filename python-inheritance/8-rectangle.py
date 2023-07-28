de904bdce83408afb2b4232399753ccd87d776bd
josephine324 <j.mutesi@alustudent.com>
2023-07-28 02:34:11 -0700

    qns 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-Rectangle = __import__('8-rectangle').Rectangle
+"""Inherits BaseGeometry class documented now"""
 
-r = Rectangle(3, 5)
 
-print(r)
-print(dir(r))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+class Rectangle(BaseGeometry):
+    """Class Rectangle"""
+
+    def __init__(self, width, height):
+        """Initialize data."""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
