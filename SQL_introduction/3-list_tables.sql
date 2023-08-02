026dfc83a53474c0894779bbf4f1d6810996ecce
josephine324 <j.mutesi@alustudent.com>
2023-08-02 02:07:30 -0700

    3-list_tables.sql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
--- This will list all tables of the database
-USE mysql;
-SHOW tables;
+-- A script that lists all the tables of a database in your MySQL server
+-- Query to list all the tables of a database
+SHOW TABLES;
