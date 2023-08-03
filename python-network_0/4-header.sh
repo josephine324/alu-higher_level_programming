d0651feface7e4f5ff4b02b47a3527b7a619b3b9
josephine324 <j.mutesi@alustudent.com>
2023-08-03 10:30:33 -0700

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send the GET request with the specified header and display the body of the response
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
