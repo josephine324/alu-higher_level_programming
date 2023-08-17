4c0e8ec2f27edbbe60c159e2e846c03effef7d0b
josephine324 <j.mutesi@alustudent.com>
2023-08-17 04:33:53 -0700

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 """
-accepts 3 arguments (mysql username, password and database name)
-and lists all cities from that database
+    A script that lists all cities from the database hbtn_0e_4_usa
 """
+
+
 import sys
 import MySQLdb
 
 
-def main(argv):
-    """connects to a given mysql database and lists all 'cities' from it"""
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states\
-                ON cities.state_id = states.id ORDER BY cities.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
+    cursor = conn.cursor()
+
+    sql = """SELECT c.id, c.name, s.name
+          FROM states s, cities c
+          WHERE c.state_id = s.id
+          ORDER BY c.id ASC"""
+
+    cursor.execute(sql)
+    cities = cursor.fetchall()
+
+    for city in cities:
+        print(city)
+
+    cursor.close()
     conn.close()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 4:
-        main(sys.argv)
